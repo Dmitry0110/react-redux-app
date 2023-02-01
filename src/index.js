14e873e34c69c17a714ea269b4502566783ddfd6
Dmitry Nefedov <nefedov0110@mail.ru>
2023-02-01 18:25:11 +0300

    comment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import * as actions from "./store/actions";
+import { initiateStore } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = initiateStore();
+
+const App = (second) => {
+  const [state, setState] = useState(store.getState());
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
+
+  const completeTask = (taskId) => {
+    store.dispatch(actions.taskCompleted(taskId));
+  };
+  const changeTitle = (taskId) => {
+    store.dispatch(actions.titleChanged(taskId));
+  };
+
+  const deleteTitle = (taskId) => {
+    store.dispatch(actions.titleDelete(taskId));
+  };
+
+  return (
+    <>
+      <h1> App</h1>
+
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p>{`Completed:${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Completed</button>
+            <button onClick={() => changeTitle(el.id)}>change Title</button>
+            <button onClick={() => deleteTitle(el.id)}>Delete Title</button>
+
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/store/taskReducer.js
@@ -0,0 +1,28 @@
+import { taskDeleted, taskUpdated } from "./actionTypes";
+
+export function taskReducer(state = [], action) {
+  switch (action.type) {
+    case taskUpdated: {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex] = {
+        ...newArray[elementIndex],
+        ...action.payload,
+      };
+      return newArray;
+    }
+    case taskDeleted: {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray.splice(elementIndex, 1);
+      return newArray.filter((element) => element !== action.payload);
+    }
+
+    default:
+      return state;
+  }
+}
